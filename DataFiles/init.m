c75ccae66b78651c7acc41708e250031e087ae1d
Jay Wadekar <jayshine12@gmail.com>
2021-04-28 11:50:13 -0400

    Usual

---
+++ DataFiles/init.m
@@ -0,0 +1,10 @@
+(* ::Package:: *)
+
+SetOptions[{Plot, LogPlot, LogLogPlot, LogLinearPlot, ListPlot, 
+   ListLogPlot, ListLogLogPlot, ListLogLinearPlot,ContourPlot}, 
+  {BaseStyle -> {FontFamily -> "Times", FontSize -> 15},GridLines->None,GridLinesStyle->Lighter[LightGray]}];
+
+
+hbar=1.054 10^-27;kb=1.38 10^-16; me = 9.1 10^-28; mp=1.67 10^-24;mgev=1.78 10^-24;
+cv=3 10^10; G=6.67 10^-8; kpc=3.086 10^21; pc=3.086 10^18; msun=1.989 10^33; 
+icm=1.98 10^-14; kelvin=8.62 10^-14;
